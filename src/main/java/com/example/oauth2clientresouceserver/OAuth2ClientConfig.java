7b845dd0678a9de44c11563885dc1ddfc9aee99e
Thomas Harper <thomas.l.harper.ctr@navy.mil>
2019-02-27 16:23:46 -0800

    add oauth2 support for webclient

--- src/main/java/com/example/oauth2clientresouceserver/OAuth2ClientConfig.java
+++ src/main/java/com/example/oauth2clientresouceserver/OAuth2ClientConfig.java
@@ -1,43 +1,56 @@
 package com.example.oauth2clientresouceserver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableWebSecurity
 @Configuration
 public class OAuth2ClientConfig extends WebSecurityConfigurerAdapter {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuerUri;
+//    @Autowired
+//    ClientRegistrationRepository clientRepo;
 
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return JwtDecoders.fromOidcIssuerLocation(issuerUri);
-    }
+//    @Bean
+//    WebClient webClient(ReactiveClientRegistrationRepository clientRegistrationRepository,
+//                        ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+//        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth =
+//                new ServerOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository, authorizedClientRepository);
+//        return WebClient.builder()
+//                .filter(oauth)
+//                .build();
+//    }
+//
+//
+//    @Bean
+//    ReactiveClientRegistrationRepository clientRegistrations() {
+//        ClientRegistration keycloak = clientRepo.findByRegistrationId("keycloak"); //todo should this be a property
+//        return new InMemoryReactiveClientRegistrationRepository(keycloak);
+//    }
 
-    @Bean
-    WebClient webClient(ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientRepository authorizedClientRepository) {
-        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository, authorizedClientRepository);
-        oauth2.setDefaultOAuth2AuthorizedClient(true);
-        return WebClient.builder()
-                .apply(oauth2.oauth2Configuration())
-                .build();
-    }
+
+//    @Bean
+//    WebClient webClient(ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientRepository authorizedClientRepository) {
+//
+//        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository, authorizedClientRepository);
+//        oauth2.setDefaultOAuth2AuthorizedClient(true);
+//        return WebClient.builder()
+//                .apply(oauth2.oauth2Configuration())
+//                .build();
+//    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.oauth2Client();
+
         http
                 .csrf().disable()
                 .anonymous().disable()
@@ -51,4 +64,5 @@ public class OAuth2ClientConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .oauth2ResourceServer().jwt();
     }
+
 }
